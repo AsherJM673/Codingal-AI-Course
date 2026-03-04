9e647e7947384f8f428efe5e9395b22c833cb37d
AsherJM673 <136257006+AsherJM673@users.noreply.github.com>
2026-03-04 17:48:14 +0000

    Add image processing and text extraction features
    Implement OCR functionality to load, preprocess, and extract text from images, with an option to save the extracted text to a file.

---
+++ Module 4/L30/main.py
@@ -0,0 +1,61 @@
+import cv2
+import pytesseract
+import os
+from datetime import datetime
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def load_image(image_path):
+    if not os.path.exists(image_path):
+        raise FileNotFoundError("Image path doesn't exist")
+    image = cv2.imread(image_path)
+    if image is None:
+        raise ValueError("Unsupported format or bad image ")
+    return image_path
+
+def preprocess_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _ , thresh = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY)
+    return thresh 
+
+def extract_text(processed_image):
+    config = "--psm 6"
+    text = pytesseract.image_to_string(processed_image, config=config)
+    return text.strip()
+
+def save_text_to_file(text, directory="output"):
+    os.makedirs(directory, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = os.path.join(directory, f"extracted_text_{timestamp}.txt")
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text)
+        return file_path
+
+def main():
+    try:
+        image_path = input("Please Enter image path: ").strip()
+        image = load_image(image_path)
+        processed_image = preprocess_image(image)
+        text = extract_text(processed_image)
+
+        if not text:
+            print("No text detected")
+            return
+        
+        print("\n--- Extracted Text ---\n")
+        print(text)
+        print("\n----------------------\n")
+
+        choice = input("Save text as .txt file (y/n)").lower().strip()
+        if choice == "y":
+            file_path = save_text_to_file(text)
+            print(f"Text saved at: {file_path}")
+        else:
+            print("Text not saved")
+
+    except Exception as e:
+        print(f"Process failed {e}")
+
+if __name__ == "__main__":
+    main()
